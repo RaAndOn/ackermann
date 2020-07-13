5bd5c437fd7b4ed1e2ed1b914d71e2233eb2448c
Joshua Ra'anan <jraanan@andrew.cmu.edu>
2020-07-13 16:59:49 -0400

    Create ackermann_controller package

---
+++ src/ackermann_controller/src/ackermann_controller_node.cpp
@@ -0,0 +1,20 @@
+#include <ros/ros.h>
+
+#include <ackermann_controller/pure_pursuit.hpp>
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "controller_node");
+  ros::NodeHandle privateNH("~");
+  ros::NodeHandle publicNH("");
+
+  //   PurePursuit purePursuit(privateNH, publicNH);
+
+  ros::Rate loop_rate(20);
+  ROS_INFO("Init controller node\n");
+  while (ros::ok()) {
+    // latticePlanner.visualizationLoopTEST();
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
